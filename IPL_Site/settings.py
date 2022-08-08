715684a58bd65766a2eb955fbc27507d93d989d0
MansiKamthan <mkamthan@unomaha.edu>
2022-08-07 23:42:19 -0500

    Changing server host to all for emails to work

--- IPL_Site/settings.py
+++ IPL_Site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v@@io2i__+#emdyb5wjk78cp2j43dg^nupvh)s4#6nahn^(gn&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 #Email
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
